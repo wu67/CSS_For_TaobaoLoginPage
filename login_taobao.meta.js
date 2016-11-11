ed0d93c647c3a997fdf3664f5ea2a5d6e375e6f3
4thleaf <wk7113@gmail.com>
2016-11-11 16:48:23 +0800

    解决二维码无法被同时显示 淘宝更新后二维码无法同时被显示出来，只使用样式的话，需要点击按钮手动切换 添加 login_taobao.user.js以配合样式使用

---
+++ login_taobao.meta.js
@@ -0,0 +1,17 @@
+/**
+ * Created by wk on 16/11/11.
+ */
+// ==UserScript==
+// @name        淘宝 登录页 taobao login
+// @namespace   wk7113@gmail.com
+// @description 页面加载完成后自动加载二维码
+// @updateURL   https://github.com/4thleaf/CSS_For_TaobaoLoginPage/blob/master/login_taobao.meta.js
+// @downloadURL https://github.com/4thleaf/CSS_For_TaobaoLoginPage/blob/master/login_taobao.user.js
+// @include     https://login.taobao.com/*
+// @icon        http://himg.baidu.com/sys/portraitl/item/da35115e?t=1460692207
+// @exclude     %exclude%
+// @license     MIT
+// @version     1
+// @grant       none
+// @author      与你偶遇的树下
+// ==/UserScript==
