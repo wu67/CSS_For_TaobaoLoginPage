73f9b8340b4ce6d733816c410efdfb5ff7dc123c
4thleaf <wk7113@gmail.com>
2016-11-17 13:54:11 +0800

    修正 @updateURL @downloadURL

--- login_taobao.user.js
+++ login_taobao.user.js
@@ -5,8 +5,8 @@
 // @name        淘宝 登录页 taobao login
 // @namespace   wk7113@gmail.com
 // @description 页面加载完成后自动加载二维码
-// @updateURL   https://github.com/4thleaf/CSS_For_TaobaoLoginPage/blob/master/login_taobao.meta.js
-// @downloadURL https://github.com/4thleaf/CSS_For_TaobaoLoginPage/blob/master/login_taobao.user.js
+// @updateURL   https://github.com/4thleaf/CSS_For_TaobaoLoginPage/raw/master/login_taobao.meta.js
+// @downloadURL https://github.com/4thleaf/CSS_For_TaobaoLoginPage/raw/master/login_taobao.user.js
 // @include     https://login.taobao.com/*
 // @icon        http://himg.baidu.com/sys/portraitl/item/da35115e?t=1460692207
 // @exclude     %exclude%
